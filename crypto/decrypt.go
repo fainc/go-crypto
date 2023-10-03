edc432963dc6c6323c41f0268103f1583ea5f8f1
lin <zx@lendata.net>
2023-10-03 22:58:40 +0800

    fixed sth

--- crypto/decrypt.go
+++ crypto/decrypt.go
@@ -9,30 +9,30 @@ import (
 )
 
 // EasyDecrypt 数据解密便捷代理方法 option可传证书密码或自定义IV,algo 支持algo列表 algorithm.SupportedAlgo
-func EasyDecrypt(algo, key, data string, hex bool, options ...string) (decrypted string, err error) {
+func EasyDecrypt(algo, secret, data string, hex bool, options ...string) (decrypted string, err error) {
 	option := ""
 	if len(options) == 1 && options[0] != "" {
 		option = options[0]
 	}
 	switch algo {
 	case "SM2_C1C3C2":
-		return gm.Sm2().Decrypt(key, option, data, 0, hex)
+		return gm.Sm2().Decrypt(secret, option, data, 0, hex)
 	case "SM2_C1C2C3":
-		return gm.Sm2().Decrypt(key, option, data, 1, hex)
+		return gm.Sm2().Decrypt(secret, option, data, 1, hex)
 	case "SM2_ASN1":
-		return gm.Sm2().DecryptAsn1(key, option, data, hex)
+		return gm.Sm2().DecryptAsn1(secret, option, data, hex)
 	case "SM4_ECB":
-		return gm.Sm4().Decrypt(key, data, "ECB", hex)
+		return gm.Sm4().Decrypt(secret, data, "ECB", hex)
 	case "SM4_CBC":
-		return gm.Sm4().Decrypt(key, data, "CBC", hex)
+		return gm.Sm4().Decrypt(secret, data, "CBC", hex)
 	case "SM4_CFB":
-		return gm.Sm4().Decrypt(key, data, "CFB", hex)
+		return gm.Sm4().Decrypt(secret, data, "CFB", hex)
 	case "SM4_OFB":
-		return gm.Sm4().Decrypt(key, data, "OFB", hex)
+		return gm.Sm4().Decrypt(secret, data, "OFB", hex)
 	case "RSA_PKCS1":
-		return rsa.DecryptPKCS1(key, data, hex)
+		return rsa.DecryptPKCS1(secret, data, hex)
 	case "AES_CBC_PKCS7":
-		return aes.CBC().DecryptPKCS7(key, data, option)
+		return aes.CBC().DecryptPKCS7(secret, data, option)
 	default:
 		return "", errors.New("unsupported algo")
 	}
--- crypto/encrypt.go
+++ crypto/encrypt.go
@@ -9,30 +9,30 @@ import (
 )
 
 // EasyEncrypt 数据加密便捷代理方法,options支持自定义AES IV,支持algo列表: algorithm.SupportedAlgo
-func EasyEncrypt(algo, key, data string, hex bool, options ...string) (encrypted string, err error) {
+func EasyEncrypt(algo, secret, data string, hex bool, options ...string) (encrypted string, err error) {
 	option := ""
 	if len(options) == 1 && options[0] != "" {
 		option = options[0]
 	}
 	switch algo {
 	case "SM2_C1C3C2":
-		return gm.Sm2().Encrypt(key, data, 0, hex)
+		return gm.Sm2().Encrypt(secret, data, 0, hex)
 	case "SM2_C1C2C3":
-		return gm.Sm2().Encrypt(key, data, 1, hex)
+		return gm.Sm2().Encrypt(secret, data, 1, hex)
 	case "SM2_ASN1":
-		return gm.Sm2().EncryptAsn1(key, data, hex)
+		return gm.Sm2().EncryptAsn1(secret, data, hex)
 	case "SM4_ECB":
-		return gm.Sm4().Encrypt(key, data, "ECB", hex)
+		return gm.Sm4().Encrypt(secret, data, "ECB", hex)
 	case "SM4_CBC":
-		return gm.Sm4().Encrypt(key, data, "CBC", hex)
+		return gm.Sm4().Encrypt(secret, data, "CBC", hex)
 	case "SM4_CFB":
-		return gm.Sm4().Encrypt(key, data, "CFB", hex)
+		return gm.Sm4().Encrypt(secret, data, "CFB", hex)
 	case "SM4_OFB":
-		return gm.Sm4().Encrypt(key, data, "OFB", hex)
+		return gm.Sm4().Encrypt(secret, data, "OFB", hex)
 	case "RSA_PKCS1":
-		return rsa.EncryptPKCS1(key, data, hex)
+		return rsa.EncryptPKCS1(secret, data, hex)
 	case "AES_CBC_PKCS7":
-		return aes.CBC().EncryptPKCS7(key, data, option)
+		return aes.CBC().EncryptPKCS7(secret, data, option)
 	default:
 		return "", errors.New("unsupported algo")
 	}
